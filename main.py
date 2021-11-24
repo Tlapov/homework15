c30253322cf3b4527d584082be85381379047dd4
Tlapov <toni.lapov95@gmail.com>
2021-11-24 17:39:15 +0100

    Prva verzija aplikacije:

--- main.py
+++ main.py
@@ -4,12 +4,16 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
-def index():
-    return render_template("index.html")
+def boogle():
+    return render_template("boogle.html")
 
-@app.route("/portfolio")
-def portfolio():
-    return render_template("portfolio.html")
+@app.route("/Fakebook")
+def Fakebook():
+    return render_template("Fakebook.html")
+@app.route("/bootstrap")
+def Bootstrap():
+    return render_template("bootstrap.html")
+
 
 if __name__ == "__main__":
     app.run(use_reloader=True)
